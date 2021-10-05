e5747cc31da5d3c9195b487d3fdae57af017a6a8
Ephrem Sisay <ephisismu@gmail.com>
2021-10-05 12:41:09 -0700

    furthest

--- main.h
+++ main.h
@@ -4,29 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
-#include <stddef.h>
 
-#define BUFFER_SIZE 1024
-
-/**
- * struct modifier - mofifier fields collection
- * @flags: flags field composed of ['0', ' ', '#', '+', '-']
- * @width: width field, positive number
- * @precision: precision field positive number not including '.'
- * or -1 for '*'
- * @length: length field string composed of ['h', 'l']
- * @specifier: specifier character can one of
- * ['c', 's', '%', 'd', 'i', 'b', 'u', 'o', 'x', 'X', 'S', 'p', 'r', 'R']
- *
- */
-typedef struct modifier
-{
-	char *flags;
-	int width;
-	int precision;
-	char *length;
-	char specifier;
-} modifier_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
