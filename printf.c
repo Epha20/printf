16e8753f47ce5342cc3289fc0def239a58b11aaa
Ephrem Sisay <ephisismu@gmail.com>
2021-10-05 03:43:48 -0700

    update

--- printf.c
+++ printf.c
@@ -1,31 +1,85 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "main.h"
 
+void _putchar_c(char c)
+{
+	write(1, &c, 1);
+}
+
+void _putchar(va_list a)
+{
+	char c;
+
+	c = va_arg(a, int);
+	write(1, &c, 1);
+}
+
+void print_str(va_list a)
+{
+	char *c;
+
+	c = va_arg(a, char *);
+	while (*c != '\0')
+	{
+		_putchar_c(*c++);
+	}
+}
+
+void print_int()
+{
+	
+}
+
+
 /**
- * _printf - print string as printf
- * @format: number of arguments
- * Return: void
- **/
+ * _printf - prints output according to a format.
+ * @format: input string.
+ * Return: int, number of characters printed,
+ */
+
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	unsigned int i, result;
+	va_list a;
+	int i, j, count;
 
-	result = 0;
-	va_start(ap, format);
+	cs_t cspec[] = {
+		{'c', _putchar},
+		{'s', print_str},
+		{'d', print_int},
+		{'i', print_int}
+	};
 
-	for (i = 0; format[i] != '\0'; i++)
+	if (format == NULL)
+		return (0);
+
+	i = j = count = 0;
+	va_start(a, format);
+	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			result += pull_print(format[i + 1], &ap);
+			j = 0;
+			while (j < 4)
+			{
+				if (format[i + 1] == cspec[j].cs &&
+				    format[i + 1] != '%')
+				{
+					cspec[j].f(a);
+					i++;
+				}
+				j++;
+			}
 			i++;
+			_putchar_c(format[i]);
 		}
 		else
-		{
-			_putchar(format[i]);
-			result++;
-		}
+			_putchar_c(format[i]);
+		count++;
+		i++;
+
 	}
-	va_end(ap);
-	return (result);
+	return (count);
 }
