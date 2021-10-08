c5f5c8a4efc8457105d88f8d9fb552726e9b3c4a
Ephrem Sisay <ephisismu@gmail.com>
2021-10-08 04:10:28 -0700

    more

--- _printf.c
+++ _printf.c
@@ -5,7 +5,6 @@
  * @format: a string composed of zero or more characters to print or use as
  * directives that handle subsequent arguments and special characters.
  * Description: This function can take a variable number and type of arguments
- * that should be printed to standard output.
  * Return: int
  */
 int _printf(const char *format, ...)
